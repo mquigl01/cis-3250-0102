82010379fe34a0d241385d62d25df589380f288f
Mehak Bedi <Mehak@Mehaks-MacBook-Pro.local>
2018-10-19 15:33:53 -0400

    fixed

--- src/wordChecker.c
+++ src/wordChecker.c
@@ -9,8 +9,8 @@ char getLetter( int i, int j, struct rolledDice **gameBoard ){
 }
 
 /* Revised by Josh Aidelman and Jaden Walt on October 4th at 4:11 PM */
+/* Comments added by Josh Aidelman on October 17th at 11:51 AM */
 int abidesRules(  int i, int j, char *word, struct rolledDice** gameBoard, int subLen, int **visited  ){
-<<<<<<< HEAD
     
     int adjCell;
     int newX;
@@ -25,39 +25,6 @@ int abidesRules(  int i, int j, char *word, struct rolledDice** gameBoard, int s
     //Checks the word to make sure it's long enough
     if(  subLen == (  strlen(  word  ) -1  )  ){
         
-=======
-
-  int adjCell;
-  int newX;
-  int newY;
-  char findLetter;
-
-  // right, digUpRight, up, digUpLeft, left, digDownLeft, down, digDownRight
-  int allX[ ] = { 0, -1, -1, -1, 0, 1, 1, 1 };
-  int allY[ ] = { 1, 1, 0, -1, -1, -1, 0, 1 };
-
-  if(  subLen == (  strlen(  word  ) -1  )  ){
-
-    return 1;
-  }
-
-  findLetter = word[ subLen + 1 ];
-  int result = 0;
-
-  for( adjCell = 0; adjCell < 8; adjCell++ ){
-
-    newX = i + allX[ adjCell ];
-    newY = j + allY[ adjCell ];
-
-    if( ( newX >= 0 ) && ( newX < 4 ) && ( newY >=0 ) && ( newY < 4 )
-    && toupper( findLetter ) == getLetter( newX, newY, gameBoard ) && !visited[ newX ][ newY ] ){
-
-      visited[ newX ][ newY ] = 1;
-      ++subLen;
-      result = abidesRules( newX, newY, word, gameBoard, subLen, visited );
-
-      if( result ){
->>>>>>> parent of 01eec49... Commented wordChecker files
         return 1;
     }
     
@@ -93,8 +60,8 @@ int abidesRules(  int i, int j, char *word, struct rolledDice** gameBoard, int s
 }
 
 /* Revised by Josh Aidelman and Jaden Walt on October 5th at 2:53 PM */
+/* Comments added by Josh Aidelman on October 17th at 12:01 AM */
 int wordChecker( struct rolledDice **gameBoard, char *word ){
-<<<<<<< HEAD
     
     int m;
     int n;
@@ -151,60 +118,6 @@ int wordChecker( struct rolledDice **gameBoard, char *word ){
     
     return 0;
     
-=======
-
-  int m;
-  int n;
-  int **visited;
-
-  visited = malloc( sizeof( int *  ) * 4 );
-
-  visited[ 0 ] = malloc( sizeof( int ) * 4 );
-  visited[ 1 ] = malloc( sizeof( int ) * 4 );
-  visited[ 2 ] = malloc( sizeof( int ) * 4 );
-  visited[ 3 ] = malloc( sizeof( int ) * 4 );
-
-  for( m = 0; m < 4; m++ ){
-
-    for( n = 0; n < 4; n++ ){
-
-      if( m == 0 && n == 0 ){
-
-        visited[ m ][ n ] = 1;
-      }
-      else{
-        visited[ m ][ n ] = 0;
-      }
-
-    }
-
-  }
-
-  for( int row = 0; row < 4; row++ ){
-
-    for ( int col = 0; col < 4; col++ ) {
-
-      if( abidesRules( row, col, word, gameBoard, 0, visited ) ){
-
-        free( visited[ 0 ] );
-        free( visited[ 1 ] );
-        free( visited[ 2 ] );
-        free( visited[ 3 ] );
-        return 1;
-      }
-
-    }
-
-  }
-
-  free( visited[ 0 ] );
-  free( visited[ 1 ] );
-  free( visited[ 2 ] );
-  free( visited[ 3 ] );
-
-  return 0;
-
->>>>>>> parent of 01eec49... Commented wordChecker files
 }
 
 /* Revised by Josh Aidelman and Jaden Walt on October 4th at 4:18 PM */
@@ -214,8 +127,8 @@ char testGetLetter( int i, int j, char **boggle ){
 
 
 /* Revised by Josh Aidelman and Jaden Walt on October 4th at 4:22 PM */
+/* Comments added by Josh Aidelman on October 17th at 12:04 AM */
 int testAbidesRules( int i, int j, char *word, char **gameBoard, int subLen, int **visited ){
-<<<<<<< HEAD
     
     int adjCell;
     int result;
@@ -231,54 +144,6 @@ int testAbidesRules( int i, int j, char *word, char **gameBoard, int subLen, int
     //Makes sure it's long enough
     if( subLen == ( strlen( word )-1 ) ){
         
-=======
-
-  int adjCell;
-  int result;
-  int newX;
-  int newY;
-  int allX[ ] = { 0, -1, -1, -1, 0, 1, 1, 1 };
-  int allY[ ] = { 1, 1, 0, -1, -1, -1, 0, 1 };
-  char findLetter;
-
-  // use subLen as index to see what letter to find
-  char currentLetter = word[ subLen ];
-
-  if( subLen == ( strlen( word )-1 ) ){
-
-    return 1;
-
-  }
-
-  if( toupper( currentLetter ) == 'Q' && toupper( word[ subLen + 2 ] ) == testGetLetter( i,j,gameBoard ) ){
-    findLetter = word[ subLen + 3 ];
-    subLen++;
-    subLen++;
-    currentLetter = word[ subLen ];
-  }
-
-  else{
-    findLetter = word[ subLen + 1 ];
-  }
-
-  result = 0;
-
-  for( adjCell = 0; adjCell < 8; adjCell++ ){
-
-    newX = i + allX[ adjCell ];
-    newY = j + allY[ adjCell ];
-
-    if( ( newX >= 0 ) && ( newX < 4 ) && ( newY >=0 ) && ( newY < 4 )
-    && toupper( findLetter ) == testGetLetter( newX, newY, gameBoard )
-    && !visited[ newX ][ newY ] ){
-
-      visited[ newX ][ newY ] = 1;
-      ++subLen;
-      result = testAbidesRules( newX, newY, word, gameBoard, subLen, visited );
-
-      if( result ){
-
->>>>>>> parent of 01eec49... Commented wordChecker files
         return 1;
         
     }
@@ -329,8 +194,8 @@ int testAbidesRules( int i, int j, char *word, char **gameBoard, int subLen, int
 }
 
 /* Revised by Josh Aidelman and Jaden Walt on October 4th at 4:28 PM */
+/* Comments added by Josh Aidelman on October 17th at 12:08 AM */
 int testWordChecker( char **boggle, char *word ){
-<<<<<<< HEAD
     
     int m;
     int n;
@@ -383,58 +248,11 @@ int testWordChecker( char **boggle, char *word ){
     free( visited[ 3 ] );
     
     return 0;
-=======
-
-  int m;
-  int n;
-  int **visited;
-  visited = malloc( sizeof( int *  ) * 4 );
-
-  visited[ 0 ] = malloc( sizeof( int ) * 4 );
-  visited[ 1 ] = malloc( sizeof( int ) * 4 );
-  visited[ 2 ] = malloc( sizeof( int ) * 4 );
-  visited[ 3 ] = malloc( sizeof( int ) * 4 );
-
-  for( m = 0; m < 4; m++ ){
-    for( n = 0; n < 4; n++ ){
-
-      if( m == 0 && n == 0 ){
-        visited[ m ][ n ] = 1;
-      }
-
-      else{
-        visited[ m ][ n ] = 0;
-      }
-    }
-  }
-  for ( int row = 0; row < 4; row++ ) {
-
-    for ( int col = 0; col < 4; col++ ) {
-
-      if( testAbidesRules( row, col, word, boggle, 0, visited ) ){
-        free( visited[ 0 ] );
-        free( visited[ 1 ] );
-        free( visited[ 2 ] );
-        free( visited[ 3 ] );
-        return 1;
-
-      }
-    }
-
-  }
-
-  free( visited[ 0 ] );
-  free( visited[ 1 ] );
-  free( visited[ 2 ] );
-  free( visited[ 3 ] );
-
-  return 0;
->>>>>>> parent of 01eec49... Commented wordChecker files
 }
 
 /* Revised by Josh Aidelman and Jaden Walt on October 4th at 4:20 PM */
+/* Comments added by Josh Aidelman on October 17th at 12:11 AM */
 int hcWordChecker( char boggle[  ] [ 4 ], char *word ){
-<<<<<<< HEAD
     
     int m;
     int n;
@@ -487,61 +305,11 @@ int hcWordChecker( char boggle[  ] [ 4 ], char *word ){
     
     return 0;
     
-=======
-
-  int m;
-  int n;
-  int **visited;
-
-  visited = malloc( sizeof( int *  ) * 4 );
-  visited[ 0 ] = malloc( sizeof( int ) * 4 );
-  visited[ 1 ] = malloc( sizeof( int ) * 4 );
-  visited[ 2 ] = malloc( sizeof( int ) * 4 );
-  visited[ 3 ] = malloc( sizeof( int ) * 4 );
-
-  for( m = 0; m < 4; m++ ){
-    for( n = 0; n < 4; n++ ){
-
-      if( m == 0 && n == 0 ){
-        visited[ m ][ n ] = 1;
-      }
-
-      else{
-        visited[ m ][ n ] = 0;
-      }
-    }
-  }
-
-  for( int row = 0; row < 4; row++ ) {
-
-    for( int col = 0; col < 4; col++ ) {
-
-      if( hcAbidesRules( row, col, word, boggle, 0, visited ) ){
-
-        free( visited[ 0 ] );
-        free( visited[ 1 ] );
-        free( visited[ 2 ] );
-        free( visited[ 3 ] );
-
-        return 1;
-      }
-
-    }
-
-  }
-  free( visited[ 0 ] );
-  free( visited[ 1 ] );
-  free( visited[ 2 ] );
-  free( visited[ 3 ] );
-
-  return 0;
-
->>>>>>> parent of 01eec49... Commented wordChecker files
 }
 
 /* Revised by Josh Aidelman and Jaden Walt on October 4th at 4:16 PM */
+/* Comments added by Josh Aidelman on October 17th at 12:15 AM */
 int hcAbidesRules( int i, int j, char *word, char boggle[  ][ 4 ], int subLen, int **visited ){
-<<<<<<< HEAD
     
     int adjCell;
     int newX;
@@ -553,49 +321,6 @@ int hcAbidesRules( int i, int j, char *word, char boggle[  ][ 4 ], int subLen, i
     
     //Checks word length
     if( subLen == ( strlen( word )-1 ) ){
-=======
-
-  int adjCell;
-  int newX;
-  int newY;
-  int allX[  ] = {0, -1, -1, -1, 0, 1, 1, 1};
-  int allY[  ] = {1, 1, 0, -1, -1, -1, 0, 1};
-  char currentLetter = word[ subLen ];
-  char findLetter;
-
-  if( subLen == ( strlen( word )-1 ) ){
-    return 1;
-  }
-
-  if( toupper( currentLetter ) == 'Q' && toupper( word[ subLen + 2 ] ) == hcGetLetter( i,j,boggle ) ){
-    findLetter = word[ subLen + 3 ];
-    subLen++;
-    subLen++;
-    currentLetter = word[ subLen ];
-
-  }
-
-  else{
-    findLetter = word[ subLen + 1 ];
-  }
-
-  int result = 0;
-
-  for( adjCell = 0; adjCell < 8; adjCell++ ){
-
-    newX = i + allX[ adjCell ];
-    newY = j + allY[ adjCell ];
-
-    if( ( newX >= 0 ) && ( newX < 4 ) && ( newY >=0 ) && ( newY < 4 )
-    && toupper( findLetter ) == hcGetLetter( newX, newY, boggle )
-    && !visited[  newX  ][  newY  ] ){
-
-      visited[  newX  ][  newY  ] = 1;
-      ++subLen;
-      result = hcAbidesRules(  newX, newY, word, boggle, subLen, visited  );
-
-      if(  result  ){
->>>>>>> parent of 01eec49... Commented wordChecker files
         return 1;
     }
     
