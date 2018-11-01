b642e1b089e2370b8046bc946966a9c1501bb8e3
aelcheik <38265469+aelcheik@users.noreply.github.com>
2018-11-01 13:44:40 -0400

    Update src/dictionary.h
    Coding convention revised

--- src/dictionary.h
+++ src/dictionary.h
@@ -1,4 +1,4 @@
-/* Last modified Oct 5th by MacKenzie Quigley*/
+/* Last modified Nov 1st by Ali El-Cheikh Ali*/
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
@@ -6,19 +6,19 @@
 #define BIG_HASH_SIZE 20000
 #define SMALL_HASH_SIZE 100
 
-/* Last modified oct 5th by Mackenzie Quigley*/
-typedef struct node {
+/* Last modified Nov 1st by Ali El-Cheikh Ali*/
+typedef struct node{
 
 	char *key;
   struct node *next;
 
-} dictionaryStruct;
+}dictionaryStruct;
 
-/* Last modified oct 5th by Mackenzie Quigley*/
-char *copyString ( const char *string );
-unsigned formHashValue ( const char *string );
-dictionaryStruct *lookupWord ( dictionaryStruct **dictionary, int hashSize, const char *key );
-dictionaryStruct *insertWord ( dictionaryStruct **dictionary, int hashSize, const char *key );
-void freeDictionary ( dictionaryStruct **dictionary, int hashSize );
+/* Last modified Nov 1st by Ali El-Cheikh Ali*/
+char *copyString( const char *string );
+unsigned formHashValue( const char *string );
+dictionaryStruct *lookupWord( dictionaryStruct **dictionary, int hashSize, const char *key );
+dictionaryStruct *insertWord( dictionaryStruct **dictionary, int hashSize, const char *key );
+void freeDictionary( dictionaryStruct **dictionary, int hashSize );
 
 #endif
