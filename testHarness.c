3017a6bc7db9704b92ee60d13063623a01213695
jaidelman <jaidelma@uoguelph.ca>
2018-11-30 14:57:14 -0500

    Finished testHarness.c

--- testHarness.c
+++ testHarness.c
@@ -10,90 +10,130 @@ void testconvertToUpper ( char **upper, char *expected);
 
 int main(){
 
-  	int userScore = 0;
-  	char word[10];
-	char expected[10];
-	char *upper = malloc(sizeof(char) * 10);
+  int userScore = 0;
+  char word[20];
+  char expected[20];
+  char *upper = malloc(sizeof(char) * 20);
 
-	strcpy(upper, "hello");
-	strcpy(expected, "HELLO");
-	testconvertToUpper(&upper, expected);
+  /*****/
+  printf("Structured Basis Testing\n");
+  strcpy(word, "123456789");
+  testIncrementTotalScore(&userScore, 11, word);
 
-  	//This is how you run a test
-  	strcpy(word, "2 ");
-  
-  	//Parameters: userScore, expectedScore, word
-  	testIncrementTotalScore(&userScore, 0, word);
+  strcpy(word, "123");
+  testIncrementTotalScore(&userScore, 1, word);
 
-  	strcpy(word, "3  ");
-  	testIncrementTotalScore(&userScore, 1, word);
+  strcpy(word, "12");
+  testIncrementTotalScore(&userScore, 0, word);
 
-  	return 0;
+  strcpy(upper, "u");
+  strcpy(expected, "U");
+  testconvertToUpper(&upper, expected);
+
+  strcpy(upper, "U");
+  strcpy(expected, "U");
+  testconvertToUpper(&upper, expected);
+
+  /*****/
+  printf("Boundary Analysis Testing\n");
+  strcpy(word, "abcd");
+  testIncrementTotalScore(&userScore, 1, word);
+
+  strcpy(word, "abcde");
+  testIncrementTotalScore(&userScore, 2, word);
+
+  strcpy(word, "abcdef");
+  testIncrementTotalScore(&userScore, 3, word);
+
+  strcpy(word, "abcdefg");
+  testIncrementTotalScore(&userScore, 5, word);
+
+  strcpy(word, "abcdefgh");
+  testIncrementTotalScore(&userScore, 11, word);
+
+  strcpy(word, "ab");
+  testIncrementTotalScore(&userScore, 0, word);
+
+  strcpy(word, "abcdefghijklmn");
+  testIncrementTotalScore(&userScore, 11, word);
+
+  strcpy(upper, "A");
+  strcpy(expected, "A");
+  testconvertToUpper(&upper, expected);
+
+  strcpy(upper, "a");
+  strcpy(expected, "A");
+  testconvertToUpper(&upper, expected);
+
+  strcpy(upper, "?");
+  strcpy(expected, "?");
+  testconvertToUpper(&upper, expected);
+
+  return 0;
 }
 
 /* This function tests incrementTotalScore. The paramaters are a pointer to
-   the user score, the expected score for that word, and then the word. It
-   sets the user score back to zero after it runs.                      */
+the user score, the expected score for that word, and then the word. It
+sets the user score back to zero after it runs.                      */
 void testIncrementTotalScore(int *userScore, int expected, char *word){
 
   incrementTotalScore(userScore, word);
 
   if(*userScore == expected){
-    printf("SUCCESS!\n\n");
+    printf("SUCCESS!\n");
   }
   else{
-    printf("Expected: %d\nResult: %d\n\n", expected, *userScore);
+    printf("ERROR!\n");
   }
+  printf("Expected: %d\nResult: %d\n\n", expected, *userScore);
   *userScore = 0;
 }
 
 void incrementTotalScore ( int *userScore, char *word ){
 
-	int wordLen = strlen( word );
-	fprintf( stdout, "wordLen: %d\n",wordLen );
-	if( wordLen == 3 || wordLen == 4 ){
+  int wordLen = strlen( word );
 
-		*userScore += 1;
-	}
-	else if( wordLen == 5 ){
+  if( wordLen == 3 || wordLen == 4 ){
 
-		*userScore += 2;
-	}
-	else if( wordLen == 6 ){
+    *userScore += 1;
+  }
+  else if( wordLen == 5 ){
 
-		*userScore += 3;
-	}
-	else if( wordLen == 7 ){
+    *userScore += 2;
+  }
+  else if( wordLen == 6 ){
 
-		*userScore+=5;
-	}
-	else if( wordLen >= 8 ){
+    *userScore += 3;
+  }
+  else if( wordLen == 7 ){
 
-		*userScore += 11;
-	}
+    *userScore+=5;
+  }
+  else if( wordLen >= 8 ){
+
+    *userScore += 11;
+  }
 }
 
 void testconvertToUpper ( char **upper, char *expected){
 
-	*upper = convertToUpper(upper);
-	
-	if (strcmp(*upper, expected) == 0){
-		printf("SUCCESS!\n\n");
-	}
-	else{
-		printf("Expected: %s\nResult: %s\n\n", expected, *upper);
-	}
+  *upper = convertToUpper(upper);
 
+  if (strcmp(*upper, expected) == 0){
+    printf("SUCCESS!\n");
+  }
+  else{
+    printf("ERROR!\n");
+  }
+  printf("Expected: %s\nResult: %s\n\n", expected, *upper);
 }
 
 char *convertToUpper ( char **upper ){
 
-	char *upperDeref = *upper;
-	for( int count = 0; upperDeref[count]; count++ ){
+  char *upperDeref = *upper;
+  for( int count = 0; upperDeref[count]; count++ ){
 
-		upperDeref[count] = toupper( upperDeref[count] );
-	}
-	return upperDeref;
+    upperDeref[count] = toupper( upperDeref[count] );
+  }
+  return upperDeref;
 }
-
-
